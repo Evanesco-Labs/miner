0a000b2bb777c370b174223f4fb057e98cc48f66
CaleHH <caleihhmy@gmail.com>
2021-12-24 16:51:33 +0800

    optimze zkp mem

--- mine.go
+++ mine.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"runtime/debug"
 	"syscall"
 	"time"
 )
@@ -74,6 +75,7 @@ type ConfigYML struct {
 }
 
 func StartMining(ctx *cli.Context) {
+	debug.SetGCPercent(10)
 	runtime.GOMAXPROCS(1)
 	config := miner.DefaultConfig()
 
